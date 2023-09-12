5b74fd06e83cb9491b3e2499ef5e050cb650aa3c
Martin Rubli <martin@rubli.info>
2023-09-12 23:21:26 +0200

    Replace GitHub actions with local build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+
+VERSION="v1.7.2"
+GOMINVERSION="1.20"
+NAME="ghcr.io/mrubli/nebula-container"
+TAG="${VERSION#v}"
+
+docker build \
+	--build-arg VERSION=${VERSION} \
+	--build-arg GOMINVERSION=${GOMINVERSION} \
+	-t ${NAME}:${TAG} \
+	.
+docker push ${NAME}:${TAG}
